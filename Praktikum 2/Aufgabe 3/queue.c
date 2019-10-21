8c0b5bb71cd4e2dd5bea63b24f2228957b97b1d0
Jessica Brinkmann <jessica@Jessicas-MacBook-Pro.local>
2019-10-21 16:50:40 +0200

    fixed Praktikum 2 Aufgabe 3

--- Praktikum 2/Aufgabe 3/queue.c
+++ Praktikum 2/Aufgabe 3/queue.c
@@ -1,25 +1,60 @@
+
+//void enqueue(int i){
+//    queue[write] = i;
+//    if(queue[write+1] != '\0' && queue[0] != NULL)
+//        write++;
+//    else if (queue[0] != NULL)
+//        write = 0;
+//}
+//
+//int dequeue(){
+//    if (queue[oldest] == NULL)
+//        return -1;
+//    else{
+//        int temp = queue[oldest];
+//        queue[oldest] = NULL;
+//        if(queue[oldest+1]!= '\0')
+//            oldest++;
+//        else oldest = 0;
+//        return temp;
+//    }
+//}
 #include <stdlib.h>
+#include "queue.h"
 
-int queue[2];
-int oldest = 0;
-int write = 0;
+#define MAX 1
 
-void enqueue(int i){
-    queue[write] = i;
-    if(queue[write+1] != '\0' && queue[0] != NULL)
-        write++;
-    else write = 0;
+int queue[MAX];
+int  head = 0;
+int  tail = 0;
+
+void enqueue(int zahl){
+    
+    if(tail < MAX){
+        queue[tail] = zahl;
+            tail++;
+    }
+    else if (tail == MAX && queue[0] == '\0'){
+        queue[0] = zahl;
+        tail = 1;
+    }
+    else if (tail == MAX)
+    {
+        queue[tail] = zahl;
+    }
 }
 
+
 int dequeue(){
-    if (queue[oldest] == NULL)
-        return -1;
-    else{
-        int temp = queue[oldest];
-        queue[oldest] = NULL;
-        if(queue[oldest+1]!= '\0')
-            oldest++;
-        else oldest = 0;
+    if (queue[head] != '\0')
+    {
+        int temp = queue[head];
+        queue[head] = '\0';
+        if(queue[head+1] != '\0')
+            head++;
+        else
+            head = 0;
         return temp;
     }
+    else return -1;
 }
